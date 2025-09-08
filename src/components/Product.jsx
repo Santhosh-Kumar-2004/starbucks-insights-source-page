581409c3c57d352edb4d9afdb9f3443781616db5
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-08 22:35:10 +0530

    Added code fo rthe product page

---
+++ src/components/Product.jsx
@@ -0,0 +1,48 @@
+// src/components/Hero.jsx
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "../styles/Hero.css";
+
+import coffeeImage from "../assets/coffee.jpg"; // use your single image
+
+const products = [
+  { name: "Caramel Latte" },
+  { name: "Mocha Frapp", featured: true },
+  { name: "Vanilla Bean" },
+  { name: "Hazelnut Brew" },
+];
+
+const Product = () => {
+  return (
+    <section className="hero">
+      <Swiper
+        modules={[Navigation]}
+        navigation
+        spaceBetween={30}
+        slidesPerView={3}
+        centeredSlides={true}
+        className="hero-swiper"
+      >
+        {products.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div className={`card ${item.featured ? "featured" : ""}`}>
+              <img src={coffeeImage} alt={item.name} />
+              <h3>{item.name}</h3>
+              <div className="options">
+                <p>Milk: Options</p>
+                <p>Cream: Options</p>
+                <p>Toppings: Options</p>
+              </div>
+              <button className="add-btn">Add to Cart</button>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Product;
