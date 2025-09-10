fe557acb9eb563183a0342dc97abedcbab46aabc
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-10 13:20:27 +0530

    Updated inputs and added the lables

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,15 +13,15 @@ const Contact = () => {
                     <form className="contact-form">
                         <label htmlFor="name">Name</label>
                         <input type="text" placeholder="Your Name" className="contact-input" />
-                        <label htmlFor="email">Email</label>
-                        <input type="email" placeholder="Your Email" className="contact-input" />
+                        <label htmlFor="email" className="">Email</label>
+                        <input type="email" placeholder="Your Email" className="contact-input mail-lable" />
                         <button type="submit" className="submit-btn">Submit</button>
                     </form>
                 </div>
 
                 {/* Right Card - Thank You */}
                 <div className="contact-card right-card">
-                    <h2 className="contact-title">Thank You</h2>
+                    <h2 className="contact-sec">Thank You</h2>
                     <p className="contact-text">We appreciate your interest in Starbucks. Stay connected with us!</p>
                     <div className="arrow-icon">
                         <FaArrowRight />
